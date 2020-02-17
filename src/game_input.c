9770106e7232da5e6a8c5bb87322488ac040db70
JMazurkiewicz <mazkuba3@gmail.com>
2020-02-17 20:08:58 +0100

    Fixed CMake and some compilation warnings on MSVC

--- src/game_input.c
+++ src/game_input.c
@@ -3,16 +3,16 @@
 #include "game.h"
 #include <stdio.h>
 
-static bool validateInput(const Game* game, char column, char row);
-static bool validateColumn(const Game* game, char column);
-static bool validateRow(const Game* game, char row);
-static bool isPlaceFree(const Game* game, char column, char row);
-static void setFieldOnBoardToCurrentPlayer(Game* game, char column, char row);
+static bool validateInput(const Game* game, unsigned char column, unsigned char row);
+static bool validateColumn(const Game* game, unsigned char column);
+static bool validateRow(const Game* game, unsigned char row);
+static bool isPlaceFree(const Game* game, unsigned char column, unsigned char row);
+static void setFieldOnBoardToCurrentPlayer(Game* game, unsigned char column, unsigned char row);
 
 void askForInput(Game* game) {
     
     printf("Podaj litere kolumny i numer rzedu (np. \"A1\"): ");
-    char column, row;
+    unsigned char column, row;
     
     while(scanf(" %c %c", &column, &row) == 0 || !validateInput(game, toupper(column), row)) {
         printf("Nieprawidlowe wartosci, sprobuj jeszcze raz: ");
@@ -23,25 +23,25 @@ void askForInput(Game* game) {
     
 }
 
-bool validateInput(const Game* game, char column, char row) {
+bool validateInput(const Game* game, unsigned char column, unsigned char row) {
     return
         validateColumn(game, column) &&
         validateRow(game, row) &&
         isPlaceFree(game, column, row);
 }
 
-bool validateColumn(const Game* game, char column) {
+bool validateColumn(const Game* game, unsigned char column) {
     return (column >= 'A') && (column <= game->dimensions.x-1 + 'A');
 }
 
-bool validateRow(const Game* game, char row) {
+bool validateRow(const Game* game, unsigned char row) {
     return (row >= '1') && (row <= game->dimensions.y-1 + '1');
 }
 
-bool isPlaceFree(const Game* game, char column, char row) {
+bool isPlaceFree(const Game* game, unsigned char column, unsigned char row) {
     return game->gameBoard[row-'1'][column-'A'] == NO_PLAYER;
 }
 
-void setFieldOnBoardToCurrentPlayer(Game* game, char column, char row) {
+void setFieldOnBoardToCurrentPlayer(Game* game, unsigned char column, unsigned char row) {
     game->gameBoard[row-'1'][column-'A'] = game->currentPlayer;
 }
